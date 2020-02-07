fe476769d73b84f6bf7f4aa4ca4b6b58d2b4c1f9
Nikoloz Gabunia <nikoloz.gabunia@scandiweb.com>
2020-02-08 02:31:40 +0400

    Attribute migration created

---
+++ app/design/frontend/Scandiweb/pwa/Magento_Theme/web/precache-manifest.e7d8142e73178cddfcb74226d59ea198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6410fca0de8780bf198",
+    "url": "/static/version1581110586483/frontend/Scandiweb/pwa/en_US/Magento_Theme/main.css"
+  },
+  {
+    "revision": "b6410fca0de8780bf198",
+    "url": "/static/version1581110586483/frontend/Scandiweb/pwa/en_US/Magento_Theme/en_US.bundle.js"
+  },
+  {
+    "revision": "a7be4eaba79a6b7f5fc1",
+    "url": "/static/version1581110586483/frontend/Scandiweb/pwa/en_US/Magento_Theme/1.en_US.bundle.js"
+  },
+  {
+    "revision": "e0f7bfbccf2678f20a22",
+    "url": "/static/version1581110586483/frontend/Scandiweb/pwa/en_US/Magento_Theme/2.en_US.bundle.js"
+  },
+  {
+    "revision": "1f912d031a054f062932",
+    "url": "/static/version1581110586483/frontend/Scandiweb/pwa/en_US/Magento_Theme/3.en_US.bundle.js"
+  }
+];
