b0331d9eb3efae4ae8ff0200c889e7be6239edc4
Vakhtang Basilidze <vakhtang.basilidze@scandiweb.com>
2020-02-08 12:11:29 +0400

    Some of the styling added

---
+++ app/design/frontend/Scandiweb/pwa/src/app/component/Header/Header.component.js
@@ -0,0 +1,429 @@
+import PropTypes from 'prop-types';
+
+import NavigationAbstract, { DEFAULT_STATE_NAME } from 'Component/NavigationAbstract/NavigationAbstract.component';
+import SearchField from 'Component/SearchField';
+import MyAccountOverlay from 'Component/MyAccountOverlay';
+import ClickOutside from 'Component/ClickOutside';
+import CartOverlay from 'Component/CartOverlay';
+import MenuOverlay from 'Component/MenuOverlay';
+import { LOGO_MEDIA } from 'Util/Media/Media';
+import { TotalsType } from 'Type/MiniCart';
+import isMobile from 'Util/Mobile';
+import Link from 'Component/Link';
+import Logo from 'Component/Logo';
+import media from 'Util/Media';
+
+import './Header.style';
+
+export const PDP = 'pdp';
+export const POPUP = 'popup';
+export const CATEGORY = 'category';
+export const CUSTOMER_ACCOUNT = 'customer_account';
+export const CUSTOMER_SUB_ACCOUNT = 'customer_sub_account';
+export const CUSTOMER_ACCOUNT_PAGE = 'customer_account_page';
+export const HOME_PAGE = 'home';
+export const MENU = 'menu';
+export const MENU_SUBCATEGORY = 'menu_subcategory';
+export const SEARCH = 'search';
+export const FILTER = 'filter';
+export const CART = 'cart';
+export const CART_EDITING = 'cart_editing';
+export const CHECKOUT = 'checkout';
+export const CMS_PAGE = 'cms-page';
+
+export default class Header extends NavigationAbstract {
+    static propTypes = {
+        navigationState: PropTypes.object.isRequired,
+        cartTotals: TotalsType.isRequired,
+        onBackButtonClick: PropTypes.func.isRequired,
+        onCloseButtonClick: PropTypes.func.isRequired,
+        onSearchBarClick: PropTypes.func.isRequired,
+        onMenuButtonClick: PropTypes.func.isRequired,
+        onClearSearchButtonClick: PropTypes.func.isRequired,
+        onMyAccountButtonClick: PropTypes.func.isRequired,
+        onSearchBarChange: PropTypes.func.isRequired,
+        onClearButtonClick: PropTypes.func.isRequired,
+        onEditButtonClick: PropTypes.func.isRequired,
+        onMinicartButtonClick: PropTypes.func.isRequired,
+        onOkButtonClick: PropTypes.func.isRequired,
+        onCancelButtonClick: PropTypes.func.isRequired,
+        onSearchOutsideClick: PropTypes.func.isRequired,
+        onMenuOutsideClick: PropTypes.func.isRequired,
+        onMyAccountOutsideClick: PropTypes.func.isRequired,
+        onMinicartOutsideClick: PropTypes.func.isRequired,
+        isClearEnabled: PropTypes.bool.isRequired,
+        searchCriteria: PropTypes.string.isRequired,
+        header_logo_src: PropTypes.string,
+        logo_alt: PropTypes.string,
+        isLoading: PropTypes.bool
+    };
+
+    static defaultProps = {
+        logo_alt: 'ScandiPWA logo',
+        header_logo_src: '',
+        isLoading: true
+    };
+
+    stateMap = {
+        [DEFAULT_STATE_NAME]: {
+            title: true,
+            logo: true
+        },
+        [POPUP]: {
+            title: true,
+            close: true
+        },
+        [PDP]: {
+            back: true,
+            title: true
+        },
+        [CATEGORY]: {
+            back: true,
+            title: true
+        },
+        [CUSTOMER_ACCOUNT]: {
+            title: true
+        },
+        [CUSTOMER_SUB_ACCOUNT]: {
+            title: true,
+            back: true
+        },
+        [CUSTOMER_ACCOUNT_PAGE]: {
+            title: true
+        },
+        [MENU]: {
+            search: true
+        },
+        [MENU_SUBCATEGORY]: {
+            back: true,
+            title: true
+        },
+        [SEARCH]: {
+            back: true,
+            search: true
+        },
+        [CART]: {
+            title: true,
+            edit: true
+        },
+        [CART_EDITING]: {
+            ok: true,
+            title: true,
+            cancel: true
+        },
+        [FILTER]: {
+            close: true,
+            clear: true,
+            title: true
+        },
+        [CHECKOUT]: {
+            back: true,
+            title: true
+        },
+        [CMS_PAGE]: {
+            back: true,
+            title: true
+        }
+    };
+
+    renderMap = {
+        cancel: this.renderCancelButton.bind(this),
+        back: this.renderBackButton.bind(this),
+        close: this.renderCloseButton.bind(this),
+        menu: this.renderMenuButton.bind(this),
+        search: this.renderSearchField.bind(this),
+        title: this.renderTitle.bind(this),
+        logo: this.renderLogo.bind(this),
+        account: this.renderAccountButton.bind(this),
+        minicart: this.renderMinicartButton.bind(this),
+        clear: this.renderClearButton.bind(this),
+        edit: this.renderEditButton.bind(this),
+        ok: this.renderOkButton.bind(this)
+    };
+
+    renderBackButton(isVisible = false) {
+        const { onBackButtonClick } = this.props;
+
+        return (
+            <button
+              key="back"
+              block="Header"
+              elem="Button"
+              mods={ { type: 'back', isVisible } }
+              onClick={ onBackButtonClick }
+              aria-label="Go back"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+            />
+        );
+    }
+
+    renderCloseButton(isVisible = false) {
+        const { onCloseButtonClick } = this.props;
+
+        return (
+            <button
+              key="close"
+              block="Header"
+              elem="Button"
+              mods={ { type: 'close', isVisible } }
+              onClick={ onCloseButtonClick }
+              aria-label="Close"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+            />
+        );
+    }
+
+    renderMenuButton(isVisible = false) {
+        const { onMenuOutsideClick, onMenuButtonClick } = this.props;
+
+        if (isMobile.any()) return null;
+
+        return (
+            <ClickOutside onClick={ onMenuOutsideClick } key="menu">
+                <div>
+                    <button
+                      block="Header"
+                      elem="Button"
+                      mods={ { isVisible, type: 'menu' } }
+                      aria-label="Go to menu and search"
+                      aria-hidden={ !isVisible }
+                      tabIndex={ isVisible ? 0 : -1 }
+                      onClick={ onMenuButtonClick }
+                    />
+                    <MenuOverlay />
+                </div>
+            </ClickOutside>
+        );
+    }
+
+    renderSearchField(isSearchVisible = false) {
+        const {
+            searchCriteria,
+            onSearchOutsideClick,
+            onSearchBarClick,
+            onSearchBarChange,
+            onClearSearchButtonClick,
+            navigationState: { name }
+        } = this.props;
+
+        return (
+            <SearchField
+              key="search"
+              searchCriteria={ searchCriteria }
+              onSearchOutsideClick={ onSearchOutsideClick }
+              onSearchBarClick={ onSearchBarClick }
+              onSearchBarChange={ onSearchBarChange }
+              onClearSearchButtonClick={ onClearSearchButtonClick }
+              isVisible={ isSearchVisible }
+              isActive={ name === SEARCH }
+            />
+        );
+    }
+
+    renderTitle(isVisible = false) {
+        const { navigationState: { title } } = this.props;
+
+        return (
+            <h2
+              key="title"
+              block="Header"
+              elem="Title"
+              mods={ { isVisible } }
+            >
+                { title }
+            </h2>
+        );
+    }
+
+    renderLogoImage() {
+        const {
+            header_logo_src,
+            logo_alt
+        } = this.props;
+
+        console.log(this.props);
+
+        return (
+            <Logo
+              src={ media(header_logo_src, LOGO_MEDIA) }
+              alt={ logo_alt }
+            />
+        );
+    }
+
+    renderLogo(isVisible = false) {
+        const { isLoading } = this.props;
+
+        if (isLoading) return null;
+
+        return (
+            <Link
+              to="/"
+              aria-label="Go to homepage by clicking on ScandiPWA logo"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+              block="Header"
+              elem="LogoWrapper"
+              mods={ { isVisible } }
+              key="logo"
+              itemScope
+              itemType="http://schema.org/Organization"
+            >
+                <meta itemProp="legalName" content="ScandiPWA" />
+                <meta itemProp="parentOrganization" content="Scandiweb" />
+                { this.renderLogoImage() }
+            </Link>
+        );
+    }
+
+    renderAccountButton(isVisible = false) {
+        const { onMyAccountOutsideClick, onMyAccountButtonClick } = this.props;
+
+        if (isMobile.any()) return null;
+
+        return (
+            <ClickOutside onClick={ onMyAccountOutsideClick } key="account">
+                <div aria-label="My account">
+                    <button
+                      block="Header"
+                      elem="Button"
+                      mods={ { isVisible, type: 'account' } }
+                      onClick={ onMyAccountButtonClick }
+                      aria-label="Open my account"
+                    />
+                    <MyAccountOverlay />
+                </div>
+            </ClickOutside>
+        );
+    }
+
+    renderMinicartItemsQty() {
+        const { cartTotals: { items_qty } } = this.props;
+
+        if (!items_qty) {
+            return null;
+        }
+
+        return (
+            <span
+              aria-label="Items in cart"
+              block="Header"
+              elem="MinicartItemCount"
+            >
+                { items_qty }
+            </span>
+        );
+    }
+
+    renderMinicartButton(isVisible = false) {
+        const { onMinicartOutsideClick, onMinicartButtonClick } = this.props;
+
+        if (isMobile.any()) return null;
+
+        return (
+            <ClickOutside onClick={ onMinicartOutsideClick } key="minicart">
+                <div
+                  block="Header"
+                  elem="Button"
+                  mods={ { isVisible, type: 'minicart' } }
+                >
+                    <button
+                      onClick={ onMinicartButtonClick }
+                      aria-label="Minicart"
+                      block="Header"
+                      elem="MinicartButton"
+                    >
+                        { this.renderMinicartItemsQty() }
+                    </button>
+                    <CartOverlay />
+                </div>
+            </ClickOutside>
+        );
+    }
+
+    renderClearButton(isVisible = false) {
+        const { isClearEnabled, onClearButtonClick } = this.props;
+
+        return (
+            <button
+              key="clear"
+              block="Header"
+              elem="Button"
+              mods={ { type: 'clear', isVisible, isDisabled: !isClearEnabled } }
+              onClick={ onClearButtonClick }
+              aria-label="Clear"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+            />
+        );
+    }
+
+    renderEditButton(isVisible = false) {
+        const { onEditButtonClick } = this.props;
+
+        return (
+            <button
+              key="edit"
+              block="Header"
+              elem="Button"
+              mods={ { type: 'edit', isVisible } }
+              onClick={ onEditButtonClick }
+              aria-label="Clear"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+            />
+        );
+    }
+
+    renderOkButton(isVisible = false) {
+        const { onOkButtonClick } = this.props;
+
+        return (
+            <button
+              key="ok"
+              block="Header"
+              elem="Button"
+              mods={ { type: 'ok', isVisible } }
+              onClick={ onOkButtonClick }
+              aria-label="Save changes"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+            >
+                { __('OK') }
+            </button>
+        );
+    }
+
+    renderCancelButton(isVisible = false) {
+        const { onCancelButtonClick } = this.props;
+
+        return (
+            <button
+              key="cancel"
+              block="Header"
+              elem="Button"
+              mods={ { type: 'cancel', isVisible } }
+              onClick={ onCancelButtonClick }
+              aria-label="Cancel changes"
+              aria-hidden={ !isVisible }
+              tabIndex={ isVisible ? 0 : -1 }
+            >
+                { __('Cancel') }
+            </button>
+        );
+    }
+
+    render() {
+        const { navigationState: { name, isHiddenOnMobile = false } } = this.props;
+
+        return (
+            <header block="Header" mods={ { name, isHiddenOnMobile } }>
+                <nav block="Header" elem="Nav">
+                    { this.renderNavigationState() }
+                </nav>
+            </header>
+        );
+    }
+}
--- app/design/frontend/Scandiweb/pwa/src/app/component/SearchField/SearchField.component.js
+++ app/design/frontend/Scandiweb/pwa/src/app/component/SearchField/SearchField.component.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import SourceComponent from 'SourceComponent/SearchField/SearchField.component';
 import { withRouter } from 'react-router-dom';
 import { HistoryType } from 'Type/Common';
@@ -25,6 +26,80 @@ export class SearchField extends SourceComponent {
         }
     };
 
+=======
+import { PureComponent, createRef } from 'react';
+import PropTypes from 'prop-types';
+
+import ClickOutside from 'Component/ClickOutside';
+import SearchOverlay from 'Component/SearchOverlay';
+
+import './SearchField.style';
+
+class SearchField extends PureComponent {
+    static propTypes = {
+        searchCriteria: PropTypes.string,
+        onSearchBarClick: PropTypes.func.isRequired,
+        onSearchBarChange: PropTypes.func.isRequired,
+        onSearchOutsideClick: PropTypes.func.isRequired,
+        onClearSearchButtonClick: PropTypes.func.isRequired,
+        isVisible: PropTypes.bool,
+        isActive: PropTypes.bool
+    };
+
+    static defaultProps = {
+        isVisible: true,
+        isActive: true,
+        searchCriteria: ''
+    };
+
+    searchBarRef = createRef();
+
+    state = {
+        isPlaceholderVisible: true
+    };
+
+    static getDerivedStateFromProps(props) {
+        const { isActive } = props;
+        if (isActive) return null;
+        return { isPlaceholderVisible: true };
+    }
+
+    onClearSearchButtonClick(isFocusOnSearchBar = true) {
+        const { onClearSearchButtonClick } = this.props;
+        if (isFocusOnSearchBar) this.searchBarRef.current.focus();
+        onClearSearchButtonClick();
+    }
+
+    handleChange = (e) => {
+        const { target: { value } } = e;
+        const { onSearchBarChange } = this.props;
+        onSearchBarChange(e);
+
+        this.setState({ isPlaceholderVisible: value === '' });
+    };
+
+    clearSearch = () => {
+        this.onClearSearchButtonClick(false);
+    };
+
+    renderClearSearch() {
+        const { isVisible } = this.props;
+
+        return (
+            <button
+              block="Header"
+              elem="Button"
+              onClick={ this.onClearSearchButtonClick }
+              mods={ {
+                  type: 'searchClear',
+                  isVisible
+              } }
+              aria-label="Clear search"
+            />
+        );
+    }
+
+>>>>>>> Some of the styling added
     renderContent() {
         const {
             searchCriteria,
@@ -45,7 +120,10 @@ export class SearchField extends SourceComponent {
                   onChange={ this.handleChange }
                   value={ searchCriteria }
                   mods={ { isActive } }
+<<<<<<< HEAD
                   onKeyDown={ this.handleKeyDown }
+=======
+>>>>>>> Some of the styling added
                   autoComplete="off"
                 />
                 <div
@@ -61,6 +139,32 @@ export class SearchField extends SourceComponent {
             </>
         );
     }
+<<<<<<< HEAD
 }
 
 export default withRouter(SearchField);
+=======
+
+    render() {
+        const {
+            onSearchOutsideClick,
+            searchCriteria,
+            isVisible,
+            isActive
+        } = this.props;
+
+        return (
+            <div block="SearchField" mods={ { isVisible, isActive } }>
+                <ClickOutside onClick={ onSearchOutsideClick }>
+                    <div block="SearchField" elem="Wrapper">
+                        { this.renderContent() }
+                        <SearchOverlay clearSearch={ this.clearSearch } searchCriteria={ searchCriteria } />
+                    </div>
+                </ClickOutside>
+            </div>
+        );
+    }
+}
+
+export default SearchField;
+>>>>>>> Some of the styling added
